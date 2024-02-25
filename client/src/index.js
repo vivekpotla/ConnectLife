b2f7086d605642082cbbee5e5c75b46c33da3724
Vivek Potla <potlavivek@gmail.com>
2024-02-25 15:27:17 +0530

    removed chakra

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import 'bootstrap/dist/css/bootstrap.css'
-import { ChakraProvider } from '@chakra-ui/react'
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    
       <App />
-    </ChakraProvider>
+    
   </React.StrictMode>
 );
 
