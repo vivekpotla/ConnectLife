7d1a47954b42a55d2a448202cb4890ff93770084
Neeha Bathula <neehabathula@gmail.com>
2024-02-25 15:03:44 +0530

    NGO Components modified

--- client/src/Components/NGO/NgoHomePage.js
+++ client/src/Components/NGO/NgoHomePage.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import {CreateCamps} from '../Camps/CreateCamps';
-import { CreatePosts } from './CreatePosts';
+import { CreatePosts } from '../Posts/CreatePosts';
 export const NgoHomePage =()=> {
   return (
-    <div>
+    <div className='mt-36 flex justify-center'>
         <CreateCamps/>
         <CreatePosts/>
     </div>
--- client/src/Components/Posts/CreatePosts.js
+++ client/src/Components/Posts/CreatePosts.js
@@ -1,32 +1,32 @@
 import { Button } from '@material-tailwind/react'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
-import { RegisterCamp } from './RegisterCamp';
 import ReactModal from 'react-modal';
-import MapComponent from '../MapComponent';
-import Create_Camp from '../Images/Create_Camp.png'
-export const CreateCamps=()=>{
-  const [camp, setCamp] = useState(false);
+import Create_Post from '../Images/Create_Post.png'
+import { RegisterPost } from './RegisterPost';
+
+export const CreatePosts = () => {
+  const [post, setPost] = useState(false);
   const navigate = useNavigate();
   const addData = () => {
-    setCamp(false);
+    setPost(false);
   }
   return (
-    <div >
+    <div className='ml-3'>
       <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img src={Create_Camp}></img>
+      <img src={Create_Post}></img>
       <Button size="md" variant="gradient" color='red' 
-      className="select-none rounded-lg block w-full hover:scale-105 focus:scale-105 active:scale-100" onClick={() => setCamp(true)}>
-        Create Camp
+      className="select-none rounded-lg block w-full hover:scale-105 focus:scale-105 active:scale-100" onClick={() => setPost(true)}>
+        New Awareness Post
       </Button>
     </div>
       <ReactModal
-        isOpen={camp}
-        onRequestClose={() => setCamp(false)}
-        contentLabel='Register Camp'
+        isOpen={post}
+        onRequestClose={() => setPost(false)}
+        contentLabel='Create Post'
       >
         <div className="flex justify-end fixed end-16">
-          <button onClick={() => setCamp(false)} className="bg-transparent border-none text-gray-500 hover:text-gray-700 focus:outline-none">
+          <button onClick={() => setPost(false)} className="bg-transparent border-none text-gray-500 hover:text-gray-700 focus:outline-none">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -44,10 +44,7 @@ export const CreateCamps=()=>{
           </button>
         </div>
         <div className='flex flex-col-reverse md:flex-row justify-evenly mt-4'>
-          <div className=''>
-            <MapComponent />
-          </div>
-          <RegisterCamp addData={addData} />
+          <RegisterPost addData={addData} />
         </div>
       </ReactModal>
     </div>
