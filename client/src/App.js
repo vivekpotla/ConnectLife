263e9c2539309be29b3f2121537063a6dfca9a59
Neeha Bathula <neehabathula@gmail.com>
2024-02-25 23:40:50 +0530

    created camps lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Modal from 'react-modal'
-import { RegisterCamp } from './Components/Camps/RegisterCamp';
 import MapComponent from './Components/MapComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 import { Header } from './Components/Header';
 import {NgoHomePage} from './Components/NGO/NgoHomePage';
+import {CampsList} from './Components/Camps/CampsList.js';
+import {CampDetails} from './Components/Camps/CampDetails.js';
 import  ActiveAppoinments from './Components/Donor/ActiveAppointments.js'
 // Make sure to bind modal to your app
 Modal.setAppElement('#root');
@@ -18,6 +19,8 @@ function App() {
       <Routes>
         <Route path='/' element={<MapComponent />} />
         <Route path='/ngo' element={<NgoHomePage />} />
+        <Route path='/camps' element={<CampsList />} />
+        <Route path='/campdetails' element={<CampDetails />} />
         <Route path='/activeappointments' element={<ActiveAppoinments/>} />
       </Routes>
     </BrowserRouter>
