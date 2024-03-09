7fe1aaefdbce69b6637dd9a3c7efa1d09d5d4ae6
saibharadwaj4919 <117429779+saibharadwaj4919@users.noreply.github.com>
2024-03-09 19:01:30 +0530

    edited bloodbanks.js

--- client/src/Components/BloodBanks.js
+++ client/src/Components/BloodBanks.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/solid';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  MagnifyingGlassIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom'
+import { Button, Input, Typography, Menu, MenuHandler, MenuItem, MenuList } from '@material-tailwind/react';
+
 
 function BloodBanks() {
   const [telanganaBloodBanks, setTelanganaBloodBanks] = useState([]);
   const [searchDistrict, setSearchDistrict] = useState('');
-  const user = JSON.parse(localStorage.getItem('user'));
-  const [userLocation, setUserLocation] = useState({
+  const [sortBy, setSortBy] = useState('distance'); // Default sorting by distance
+  //const [query, setQuery] = useState('');
+  const userLocation = {
     latitude: 17.454242,
     longitude: 78.393940
-  });
+  };
 
   useEffect(() => {
     const BloodBanksData = async () => {
@@ -21,25 +32,32 @@ function BloodBanks() {
 
         let filteredBloodBanks = telanganaData.filter(record => {
           if (!searchDistrict) {
-            return true; 
+            return true;
           } else {
-            return record.district.toLowerCase() === searchDistrict.toLowerCase(); 
+            return record.district.toLowerCase() === searchDistrict.toLowerCase();
           }
         });
 
-        // Apply nearest logic only when searchDistrict is empty
         if (!searchDistrict) {
           filteredBloodBanks = filteredBloodBanks.filter(record => {
             const distance = calculateDistance(userLocation.latitude, userLocation.longitude, record.latitude, record.longitude);
-            return distance <= 20; 
+            return distance <= 20;
           });
         }
 
-        const sortedBloodBanks = filteredBloodBanks.sort((a, b) => {
-          const distanceA = calculateDistance(userLocation.latitude, userLocation.longitude, a.latitude, a.longitude);
-          const distanceB = calculateDistance(userLocation.latitude, userLocation.longitude, b.latitude, b.longitude);
-          return distanceA - distanceB;
-        });
+        let sortedBloodBanks;
+        if (sortBy === 'distance') {
+          sortedBloodBanks = filteredBloodBanks.sort((a, b) => {
+            const distanceA = calculateDistance(userLocation.latitude, userLocation.longitude, a.latitude, a.longitude);
+            const distanceB = calculateDistance(userLocation.latitude, userLocation.longitude, b.latitude, b.longitude);
+            return distanceA - distanceB;
+          });
+        } else if (sortBy === 'name') {
+          sortedBloodBanks = filteredBloodBanks.sort((a, b) => {
+            return a.h_name.localeCompare(b.h_name);
+          });
+        }
+
         setTelanganaBloodBanks(sortedBloodBanks);
 
       } catch (error) {
@@ -48,20 +66,20 @@ function BloodBanks() {
     };
 
     BloodBanksData();
-  }, [searchDistrict]); // Trigger the effect when searchDistrict changes
+  }, [searchDistrict, sortBy]); // Trigger the effect when searchDistrict or sortBy changes
 
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const earthRadiusKm = 6371; 
+    const earthRadiusKm = 6371;
     const dLat = degreesToRadians(lat2 - lat1);
     const dLon = degreesToRadians(lon2 - lon1);
     const radLat1 = degreesToRadians(lat1);
     const radLat2 = degreesToRadians(lat2);
 
     const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.sin(dLon / 2) * Math.sin(dLon / 2) *
-              Math.cos(radLat1) * Math.cos(radLat2);
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) *
+      Math.cos(radLat1) * Math.cos(radLat2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = earthRadiusKm * c; 
+    const distance = earthRadiusKm * c;
 
     return distance;
   };
@@ -74,24 +92,81 @@ function BloodBanks() {
     setSearchDistrict(event.target.value);
   };
 
+  const handleSortChange = (sortBy) => {
+    setSortBy(sortBy);
+  };
+
   return (
-    <div className="container mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Blood Banks in Telangana</h2>
-      <div className="mb-4">
-        <label htmlFor="districtSearch" className="block text-sm font-medium text-gray-700">Search by District:</label>
-        <input
-          type="text"
-          id="districtSearch"
-          name="districtSearch"
-          value={searchDistrict}
-          onChange={handleSearch}
-          placeholder="Enter district name"
-          className="mt-1 p-2 border rounded-md w-full"
-        />
+    <div className="mx-auto">
+    <div className='bg-gray-100'>
+      <h2 className="text-xl font-bold mb-4 pt-2 text-gray-700 text-center">Blood Banks in Telangana</h2>
+      <div className="md:flex-row flex-col flex md:mr-5 mr-2 mt-3 justify-center">
+        <div className='relative flex md:mt-0 mt-3 mb-3'>
+          <div className="relative inline-block rounded-md">
+            <div className="flex  bg-gray-100 p-1 rounded-md items-center">
+              <AdjustmentsHorizontalIcon  className="h-[18px] w-[18px] text-blue-gray-500 mr-1"  />
+              <Menu >
+                <MenuHandler>
+                  <Link size="sm" className="bg-gray-100  text-black-500 flex items-center">
+                    {sortBy === 'distance' ? 'Distance' : 'Name'}
+                    <ChevronDownIcon className="h-4 w-4 ml-1"  strokeWidth={2}/>
+                  </Link>
+
+                </MenuHandler>
+                <MenuList className="absolute left-0 z-50 mt-2 py-1">
+                  <MenuItem onClick={() => handleSortChange('distance')} className="block px-4 py-2 hover:bg-blue-gray-100 cursor-pointer">
+                    <Typography variant="h6" color="blue-gray" className="">
+                      Nearest
+                    </Typography>
+                    <Typography variant="small" color="gray" className="font-normal">
+                     Sort by distance
+                    </Typography></MenuItem>
+                  <MenuItem onClick={() => handleSortChange('name')} className="block px-4 py-2 hover:bg-blue-gray-100 cursor-pointer">
+                    <Typography variant="h6" color="blue-gray" className="">
+                      Name
+                    </Typography>
+                    <Typography variant="small" color="gray" className="font-normal">
+                      Sort by name
+                    </Typography>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </div>
+          </div>
+          <Input
+            type="text"
+            id="districtSearch"
+            name="districtSearch"
+            value={searchDistrict}
+            onChange={handleSearch}
+            placeholder="Enter district name"
+            className="!border !border-gray-300 pr-14 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-2 ring-transparent focus:ring-gray-900/10 ms-3"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "max-w-[300px] min-w-[100px]" }}
+          />
+          <Button
+            size="sm"
+            type="submit"
+            className="!absolute right-1 top-1 rounded bg-red-900 px-3 py-1.5">
+            <MagnifyingGlassIcon className='h-5 w-5 ' />
+          </Button>
+        </div>
       </div>
-      <table className="table-auto w-full">
-        <thead>
-          <tr>
+
+</div>
+
+
+
+
+
+
+<div className='container mx-auto mb-10 '>
+
+      <table className="table-auto w-full mt-3 border border-black-100">
+        <thead className='bg-gray-100'>
+          <tr >
             <th className="px-4 py-2">Name</th>
             <th className="px-4 py-2">District</th>
             <th className="px-4 py-2">Address</th>
@@ -100,7 +175,7 @@ function BloodBanks() {
         </thead>
         <tbody>
           {telanganaBloodBanks.map((bank, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
+            <tr key={index} className={index % 2 !== 0 ? 'bg-gray-100 hover:bg-blue-gray-100' : 'bg-white hover:bg-blue-gray-100' }>
               <td className="border px-4 py-2">{bank.h_name}</td>
               <td className="border px-4 py-2">{bank.district}</td>
               <td className="border px-4 py-2">{bank.address}</td>
@@ -109,6 +184,7 @@ function BloodBanks() {
           ))}
         </tbody>
       </table>
+    </div>
     </div>
   );
 }
