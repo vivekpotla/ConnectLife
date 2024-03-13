58c69dfe3ef7b20274d100ca2c3af9d5b5dfd0c0
Neeha Bathula <neehabathula@gmail.com>
2024-03-13 22:42:34 +0530

    nog previouscamps

--- client/src/Components/NGO/CampsBloodUnits.js
+++ client/src/Components/NGO/CampsBloodUnits.js
@@ -1,32 +1,18 @@
 import React from 'react';
 
-export const CampsBloodUnits = ({ show, handleClose, donors }) => {
-  if (!show) {
-    return null;
+export const CampsBloodUnits = ({ show, handleClose, selectedCampBloodUnits }) => {
+  // Check if selectedCampBloodUnits is not available yet
+  if (!selectedCampBloodUnits) {
+    return null; // Return null to prevent rendering the component
   }
 
-  const countBloodUnits = () => {
-    const bloodUnitsCount = {};
-    donors.forEach(donor => {
-      if (donor.bloodGroup in bloodUnitsCount) {
-        bloodUnitsCount[donor.bloodGroup] += donor.unitsDonated;
-      } else {
-        bloodUnitsCount[donor.bloodGroup] = donor.unitsDonated;
-      }
-    });
-    return bloodUnitsCount;
-  };
-
-  const bloodUnits = countBloodUnits();
-
+  // Once selectedCampBloodUnits is available, continue with rendering the component
   const downloadCsv = () => {
     const csvContent =
       "data:text/csv;charset=utf-8," +
       ["Blood Group", "Total Units Donated"].join(",") +
       "\n" +
-      Object.keys(bloodUnits)
-        .map(bloodGroup => `${bloodGroup},${bloodUnits[bloodGroup]}`)
-        .join("\n");
+      selectedCampBloodUnits.bloodUnits.map(bloodUnit => `${bloodUnit.bloodType},${bloodUnit.quantity}`).join("\n");
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
@@ -39,9 +25,6 @@ export const CampsBloodUnits = ({ show, handleClose, donors }) => {
     <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-4 rounded-lg max-w-md">
         <h2 className="text-xl font-semibold mb-2">Blood Units</h2>
-        {donors.length === 0 ? ( // Check if there are no donors
-          <p className="text-gray-600">No donors registered</p>
-        ) : (
         <div className="overflow-y-auto max-h-72">
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
@@ -51,22 +34,18 @@ export const CampsBloodUnits = ({ show, handleClose, donors }) => {
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-              {Object.keys(bloodUnits).map((bloodGroup, index) => (
+              {selectedCampBloodUnits.bloodUnits.map((bloodUnit, index) => (
                 <tr key={index}>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">{bloodGroup}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">{bloodUnits[bloodGroup]}</td>
+                  <td className="px-6 py-4 whitespace-nowrap text-center">{bloodUnit.bloodType}</td>
+                  <td className="px-6 py-4 whitespace-nowrap text-center">{bloodUnit.quantity}</td>
                 </tr>
               ))}
             </tbody>
           </table>
           <div className="flex justify-end mt-4">
-          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600" onClick={downloadCsv}>Download CSV</button>
-          <button className="bg-gray-500 text-white px-4 py-2 ml-2 rounded hover:bg-gray-600" onClick={handleClose}>Close</button>
-        </div>
+            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600" onClick={downloadCsv}>Download CSV</button>
+            <button className="bg-gray-500 text-white px-4 py-2 ml-2 rounded hover:bg-gray-600" onClick={handleClose}>Close</button>
           </div>
-        )}
-        <div className="mt-4">
-          <button className="bg-gray-500 text-white px-4 py-2 ml-2 rounded hover:bg-gray-600" onClick={handleClose}>Close</button>
         </div>
       </div>
     </div>
--- client/src/Components/NGO/PreviousCamps.js
+++ client/src/Components/NGO/PreviousCamps.js
@@ -1,47 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { format, isBefore } from 'date-fns'; // Import isAfter from date-fns
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { format, isBefore, isAfter } from 'date-fns';
+import { BellAlertIcon } from '@heroicons/react/24/outline';
+import { Tooltip, IconButton } from '@material-tailwind/react';
 import { CampsDonors } from './CampsDonors';
 import { CampsBloodUnits } from './CampsBloodUnits';
-import axios from 'axios';
-import { EllipsisVerticalIcon } from '@heroicons/react/24/solid';
-import { IconButton, Tooltip } from '@material-tailwind/react';
-import { BellAlertIcon } from '@heroicons/react/24/outline';
-import { clearStorage } from 'mapbox-gl';
 
 export const PreviousCamps = () => {
-
   const [campsData, setCampsData] = useState([]);
+  const [upcomingCamps, setUpcomingCamps] = useState([]);
+  const [previousCamps, setPreviousCamps] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [showDonorsModal, setShowDonorsModal] = useState(false);
   const [showBloodUnitsModal, setShowBloodUnitsModal] = useState(false);
   const [selectedCampDonors, setSelectedCampDonors] = useState([]);
   const [selectedCampBloodUnits, setSelectedCampBloodUnits] = useState([]);
+  const [notify, setNotify] = useState(false);
 
   useEffect(() => {
     const fetchCampsData = async () => {
       try {
-        await axios.post("http://localhost:5000/api/ngo/previous-camps", { ngoId: JSON.parse(localStorage.getItem("user"))._id }).then((res) => {
-          setCampsData(res.data);
-          // console.log(res.data);
-        }).catch((error) => {
-          console.log(error);
-        });
-
+        const response = await axios.get('http://localhost:5000/api/ngo/view-all-camps');
+        const allCamps = response.data;
+        setCampsData(allCamps);
+        const now = new Date();
+        const upcoming = allCamps.filter(camp => isAfter(new Date(camp.startDate), now));
+        const previous = allCamps.filter(camp => isBefore(new Date(camp.endDate), now));
+        setUpcomingCamps(upcoming);
+        setPreviousCamps(previous);
+        setLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching camps data:', error);
       }
     };
+
     fetchCampsData();
   }, []);
 
   const handleViewDonors = async (campId) => {
     try {
-      await axios.post("http://localhost:5000/api/ngo/get-donors-in-camp", { campId }).then((res) => {
-        setSelectedCampDonors(res.data.donated); // assuming the response has 'donated' field for donors who donated
-        setShowDonorsModal(true);
-        console.log(res.data);
-      }).catch((error) => {
-        console.log(error);
-      });
+      const response = await axios.post("http://localhost:5000/api/ngo/get-donors-in-camp", { campId });
+      setSelectedCampDonors(response.data.donated);
+      setShowDonorsModal(true);
     } catch (error) {
       console.error('Error fetching donors:', error);
     }
@@ -49,35 +51,50 @@ export const PreviousCamps = () => {
 
   const handleViewBloodUnits = async (campId) => {
     try {
-      await axios.post("http://localhost:5000/api/ngo/get-blood-quantity", { campId }).then((res) => {
-        setSelectedCampBloodUnits(res.data.donated); // assuming the response has 'donated' field for donors who donated
-        setShowBloodUnitsModal(true);
-        console.log(res.data);
-      }).catch((error) => {
-        console.log(error);
+      const response = await axios.post("http://localhost:5000/api/ngo/get-blood-quantity", { campId });
+      const { data } = response;
+      const { campName, campAddress, bloodQuantity } = data;
+  
+      // Transform the blood quantity object into an array of objects
+      const bloodUnitsArray = Object.entries(bloodQuantity).map(([bloodType, quantity]) => ({
+        bloodType,
+        quantity
+      }));
+  
+      setSelectedCampBloodUnits({
+        campName,
+        campAddress,
+        bloodUnits: bloodUnitsArray
       });
+      setShowBloodUnitsModal(true);
+
     } catch (error) {
-      console.error('Error fetching donors:', error);
+      console.error('Error fetching blood units:', error);
     }
   };
-
-  const handleCloseModal = () => {
-    setShowDonorsModal(false);
-    setShowBloodUnitsModal(false);
-  };
-
-  const [notify, setNotify] = useState(false);
+  console.log(selectedCampBloodUnits)
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <FontAwesomeIcon icon={faSpinner} spin size="3x" />
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4 text-center">Previous Camps</h1>
-      {campsData.length > 0 ? ( // Check if campsData is not null
+      <h1 className="text-3xl font-bold mb-4 text-center">Blood Donation Camps</h1>
+      
+      <section>
+        <h2 className="text-2xl font-semibold mb-4 text-center">Upcoming Camps</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* .filter(camp => isBefore(new Date(camp.endDate), new Date()) && isBefore(new Date(camp.startDate), new Date())) // Filter camps with endDate after today */}
-          {campsData
-            .map((camp, index) => (
-              <div key={index} className="bg-white rounded-lg shadow-md p-4 flex flex-col relative justify-between">
-                <div className='absolute right-3'>
+          {upcomingCamps.map((camp, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-md p-4 flex flex-col relative justify-between">
+              <img src={camp.imageURL} alt={camp.name} className="w-full h-48 object-cover mb-4" />
+              <h2 className="text-xl font-semibold mb-2">{camp.name}</h2>
+              <p className="text-gray-600 mb-2">Date: {format(new Date(camp.startDate), 'MM/dd/yyyy')}</p>
+              <p className="text-gray-600 mb-2 line-clamp-3">Location: {camp.location}</p>
+              <div className='absolute right-3'>
                   <Tooltip content="Send Mail to Volunteers">
                     <IconButton disabled={notify} variant='text' onClick={async () => {
                       setNotify(true);
@@ -93,24 +110,46 @@ export const PreviousCamps = () => {
                     </IconButton>
                   </Tooltip>
                 </div>
-                <div>
-                  <img src={camp.imageURL} alt={camp.name} className="w-full h-48 object-cover mb-4" />
-                  <h2 className="text-xl font-semibold mb-2">{camp.name}</h2>
-                  <p className="text-gray-600 mb-2">Date: {format(new Date(Date.parse(camp.startDate)), 'MM/dd/yyyy')}</p>
-                  <p className="text-gray-600 mb-2 line-clamp-3">Location: {camp.location}</p>
-                </div>
-                <div className='flex justify-around mt-4'>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={() => handleViewDonors(camp._id)}>View Donors</button>
-                  <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600" onClick={() => handleViewBloodUnits(camp._id)}>View Blood Units</button>
-                </div>
-              </div>
-            ))}
+            </div>
+          ))}
         </div>
-      ) : (
-        <div>Loading...</div>
-      )}
-      <CampsDonors show={showDonorsModal} handleClose={handleCloseModal} donors={selectedCampDonors} />
-      <CampsBloodUnits show={showBloodUnitsModal} handleClose={handleCloseModal} donors={selectedCampBloodUnits} />
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-semibold mb-2 mt-4  text-center">Previous Camps</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {previousCamps.map((camp, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-md p-4 flex flex-col relative justify-between">
+              <img src={camp.imageURL} alt={camp.name} className="w-full h-48 object-cover mb-4" />
+              <h2 className="text-xl font-semibold mb-2">{camp.name}</h2>
+              <p className="text-gray-600 mb-2">Date: {format(new Date(camp.startDate), 'MM/dd/yyyy')}</p>
+              <p className="text-gray-600 mb-2 line-clamp-3">Location: {camp.location}</p>
+              <div className="flex justify-around mt-4">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  onClick={() => handleViewDonors(camp._id)}
+                >
+                  View Donors
+                </button>
+                <button
+                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                  onClick={() => handleViewBloodUnits(camp._id)}
+                >
+                  View Blood Units
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <CampsDonors show={showDonorsModal} handleClose={() => setShowDonorsModal(false)} donors={selectedCampDonors} />
+      {showBloodUnitsModal && selectedCampBloodUnits && ( // Add a check for selectedCampBloodUnits
+      <CampsBloodUnits
+        show={showBloodUnitsModal}
+        handleClose={() => setShowBloodUnitsModal(false)}
+        selectedCampBloodUnits={selectedCampBloodUnits}
+      />
+    )}
     </div>
   );
-}
+};
